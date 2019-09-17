b63b5c8f059ea0e6d5218f9e6e80c3f359053bc3
zhouyubo2333 <55138249+zhouyubo2333@users.noreply.github.com>
2019-09-17 17:09:18 +0800

    Update 第二次作业.java

--- 第二次作业.java
+++ 第二次作业.java
@@ -1,1 +1,14 @@
-# -
+class Single {
+  private static Single INSTANCE=new Single();
+  private Single() {}
+  public static Single getInstance() {
+    return INSTANCE;
+  }
+  
+  class Example2{
+    public static void main (String[] args){
+      Single s1=Single.get.getInstance();
+      Single s2=Single.get.getInstance();
+      System.out.println(s1==s2);
+    }
+  }
