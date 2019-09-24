2348d50fd54c215cc5f4ca73c3a91cb10dc8be9a
zhouyubo2333 <55138249+zhouyubo2333@users.noreply.github.com>
2019-09-24 13:36:38 +0800

    Add files via upload

---
+++ 工厂模式.java
@@ -0,0 +1,52 @@
+import org.zero01.operation.Operation;
+public interface Factory {
+    public Operation createOperation() ;
+}
+
+public class AddFactory implements Factory{
+
+ 
+    public Operation createOperation() {
+
+        System.out.println("加法运算");
+
+        return new Add();
+    }
+
+}
+
+     public class SubFactory implements Factory{
+
+ 
+    public Operation createOperation() {
+
+        System.out.println("减法运算");
+
+        return new Sub();
+    }
+
+}
+
+
+public class Client {
+
+ 
+    public static void main(String[] args) throws Exception {
+
+
+        Factory addFactory = (Factory) Class.forName("org.zero01.factory.AddFactory").newInstance();
+
+        Factory subFactory=(Factory) Class.forName("org.zero01.factory.SubFactory").newInstance();
+
+
+        Operation add = addFactory.createOperation();
+
+        Operation sub = subFactory.createOperation();
+
+ 
+        System.out.println(add.getResult(1, 1));
+
+        System.out.println(sub.getResult(1, 1));
+    }
+
+}
---
+++ 第三次作业简单工厂.java
@@ -0,0 +1,114 @@
+public interface Operation {
+    public double getResult(double numberA,double numberB) throws Exception;
+}
+
+public class Add implements Operation{
+ 
+   
+
+    public double getResult(double numberA, double numberB) {
+
+ 
+        return numberA + numberB;
+    }
+
+}
+ 
+ 
+
+public class Sub implements Operation{
+ 
+    
+
+    public double getResult(double numberA, double numberB) {
+
+        return numberA-numberB;
+    }
+
+}
+ 
+ 
+
+public class Mul implements Operation{
+ 
+    
+
+    public double getResult(double numberA, double numberB) {
+        
+        return numberA * numberB;
+    }
+
+}
+ 
+ 
+
+public class Div implements Operation {
+ 
+    
+    public double getResult(double numberA, double numberB) throws Exception {
+        if (numberB == 0) {
+
+            throw new Exception("除数不能为0！");
+        }
+
+        return numberA / numberB;
+    }
+
+}
+
+public class EasyFactory {
+ 
+   
+
+    public static Operation createOperation(String name) {
+
+        Operation operationObj = null;
+        switch (name) {
+ 
+            case "+":
+               operationObj = new Add();
+                break;
+
+            case "-":
+                operationObj = new Sub();
+                break;
+
+            case "*":
+                operationObj = new Mul();
+                break;
+
+            case "/":
+                operationObj = new Div();
+                break;
+        }
+
+        return operationObj;
+    }
+
+}
+
+public class Client {
+
+ 
+    public static void main(String[] args) throws Exception {
+
+ 
+       Operation add = EasyFactory.createOperation("+");
+
+        Operation sub = EasyFactory.createOperation("-");
+
+        Operation mul = EasyFactory.createOperation("*");
+
+        Operation div = EasyFactory.createOperation("/");
+
+ 
+        System.out.println(add.getResult(1, 1));
+
+        System.out.println(sub.getResult(1, 1));
+
+        System.out.println(mul.getResult(1, 1));
+
+        System.out.println(div.getResult(1, 1));
+    }
+
+}
