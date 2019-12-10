2e92e993d3ce19b0f79161d5e99cdb4d85f330b7
zhouyubo2333 <55138249+zhouyubo2333@users.noreply.github.com>
2019-12-10 22:22:06 +0800

    Update TelcomAccountSystem.java

--- 通讯/TelcomAccountSystem.java
+++ 通讯/TelcomAccountSystem.java
@@ -1,7 +1,7 @@
 
 public class TelcomAccountSystem {
      public static void main(String[] args) {
-    	 TelcomUser telcomUser = new TelcomUser("13800138000");
+    	 TelcomUser telcomUser = new TelcomUser("15937233517");
     	 
     	 telcomUser.generateCommunicationRecord();
     	 
